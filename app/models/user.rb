27b0d5e864374c443ddb3ff9edbcf53b68800e4a
Aniket Garg <aniketgarg@outlook.com>
2016-01-12 20:22:02 +0530

    fixes 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
     if body.present? and body.first.include?("book")
       start_latitude  = body[1].include?("lat") && body[1].split(":")[1]
       start_longitude = body[2].include?("lng") && body[2].split(":")[1]
-      product_name    = body[3].include?("product") && body[3].split(":")[1]
+      product_name    = body[3].split(":")[1]
       product         = product_name.include?("any") ? Product.where("name = #{product}").last : nil
       product_id      = product.present? ? product.uber_id : nil
       book_params = {
